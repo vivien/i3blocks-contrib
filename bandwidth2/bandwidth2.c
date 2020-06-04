7a2f6f52937c6600dce203f737aa2fa6bd4f1226
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-06-04 21:35:10 +0200

    Small text label overhaul and new modules

--- bandwidth2/bandwidth2.c
+++ bandwidth2/bandwidth2.c
@@ -11,7 +11,8 @@
 
 typedef unsigned long long int ulli;
 
-enum {
+enum
+{
   STATE_OK,
   STATE_WARNING,
   STATE_CRITICAL,
@@ -34,12 +35,13 @@ void usage(char *argv[])
   printf("\n");
 }
 
-void get_values(char **const ifaces, int num_ifaces, time_t * const s, ulli * const received, ulli * const sent)
+void get_values(char **const ifaces, int num_ifaces, time_t *const s, ulli *const received, ulli *const sent)
 {
   FILE *f;
 
   f = fopen("/proc/net/dev", "r");
-  if (!f) {
+  if (!f)
+  {
     fprintf(stderr, "Can't open /proc/net/dev\n");
     exit(STATE_UNKNOWN);
   }
@@ -50,12 +52,16 @@ void get_values(char **const ifaces, int num_ifaces, time_t * const s, ulli * co
 
   *received = 0;
   *sent = 0;
-  while (fgets(line, BUFSIZ - 1, f) != NULL) {
-    if (sscanf(line, "%[^:]: %llu %*u %*u %*u %*u %*u %*u %*u %llu", ifname, &temp_r, &temp_s) == 3) {
+  while (fgets(line, BUFSIZ - 1, f) != NULL)
+  {
+    if (sscanf(line, "%[^:]: %llu %*u %*u %*u %*u %*u %*u %*u %llu", ifname, &temp_r, &temp_s) == 3)
+    {
       int i;
       int iface_found = num_ifaces == 0;
-      for (i = 0; i < num_ifaces; i++) {
-        if (strcmp(ifaces[i], ifname) != 0) {
+      for (i = 0; i < num_ifaces; i++)
+      {
+        if (strcmp(ifaces[i], ifname) != 0)
+        {
           iface_found = 1;
           break;
         }
@@ -72,40 +78,54 @@ void get_values(char **const ifaces, int num_ifaces, time_t * const s, ulli * co
   fclose(f);
 
   *s = time(NULL);
-  if (!s) {
+  if (!s)
+  {
     fprintf(stderr, "Can't get Epoch time\n");
     exit(STATE_UNKNOWN);
   }
 }
 
 void display(int const unit, int const divisor,
-             double b, int const warning, int const critical)
+             double b, int const warning, int const critical, int const decimals)
 {
   char fmtstr[7];
 
-  if (critical != 0 && b > critical) {
+  if (critical != 0 && b > critical)
+  {
     printf("<span fallback='true' color='%s'>", RED);
-  } else if (warning != 0 && b > warning) {
+  }
+  else if (warning != 0 && b > warning)
+  {
     printf("<span fallback='true' color='%s'>", ORANGE);
-  } else {
+  }
+  else
+  {
     printf("<span fallback='true'>");
   }
 
   if (unit == 'b')
     b = b * 8;
 
-  snprintf(fmtstr, sizeof (fmtstr), "%%%d.1lf", divisor > 1000 ? 6 : 5);
+  int length = decimals + divisor > 1000 ? 5 : 4 + (decimals > 0 ? 1 : 0);
+  snprintf(fmtstr, sizeof(fmtstr), "%%%d.%dlf", length, decimals);
 
-  if (b < divisor) {
+  if (b < divisor)
+  {
     printf(fmtstr, b);
     printf(" %c/s", unit);
-  } else if (b < divisor * divisor) {
+  }
+  else if (b < divisor * divisor)
+  {
     printf(fmtstr, b / divisor);
     printf("K%c/s", unit);
-  } else if (b < divisor * divisor * divisor) {
+  }
+  else if (b < divisor * divisor * divisor)
+  {
     printf(fmtstr, b / (divisor * divisor));
     printf("M%c/s", unit);
-  } else {
+  }
+  else
+  {
     printf(fmtstr, b / (divisor * divisor * divisor));
     printf("G%c/s", unit);
   }
@@ -116,10 +136,12 @@ void parse_ifaces(char *str, char ***ifaces, int *num_ifaces)
 {
   int max_ifaces = strlen(str) / 2 + 1;
 
-  *ifaces = calloc(max_ifaces, sizeof (char *));
+  *ifaces = calloc(max_ifaces, sizeof(char *));
   *num_ifaces = 0;
-  while (((*ifaces)[*num_ifaces] = strsep(&str, ","))) {
-    if ((*ifaces)[*num_ifaces] != NULL && (*ifaces)[*num_ifaces][0] != '\0') {
+  while (((*ifaces)[*num_ifaces] = strsep(&str, ",")))
+  {
+    if ((*ifaces)[*num_ifaces] != NULL && (*ifaces)[*num_ifaces][0] != '\0')
+    {
       (*num_ifaces)++;
     }
   }
@@ -135,6 +157,7 @@ int main(int argc, char *argv[])
   int divisor = 1024;
   char *envvar = NULL;
   char *label = "";
+  int decimals = 1;
 
   envvar = getenv("USE_BITS");
   if (envvar && *envvar == '1')
@@ -169,9 +192,14 @@ int main(int argc, char *argv[])
   envvar = getenv("LABEL");
   if (envvar)
     label = envvar;
+  envvar = getenv("DECIMALS");
+  if (envvar)
+    decimals = atoi(envvar);
 
-  while (c = getopt(argc, argv, "bBsht:i:w:c:"), c != -1) {
-    switch (c) {
+  while (c = getopt(argc, argv, "bBsht:i:w:c:"), c != -1)
+  {
+    switch (c)
+    {
     case 'b':
     case 'B':
       unit = c;
@@ -205,16 +233,17 @@ int main(int argc, char *argv[])
 
   get_values(ifaces, num_ifaces, &s_old, &received_old, &sent_old);
 
-  while (1) {
+  while (1)
+  {
     sleep(t);
     get_values(ifaces, num_ifaces, &s, &received, &sent);
 
     rx = (received - received_old) / (float)(s - s_old);
     tx = (sent - sent_old) / (float)(s - s_old);
     printf("%s", label);
-    display(unit, divisor, rx, warningrx, criticalrx);
+    display(unit, divisor, rx, warningrx, criticalrx, decimals);
     printf(" ");
-    display(unit, divisor, tx, warningtx, criticaltx);
+    display(unit, divisor, tx, warningtx, criticaltx, decimals);
     printf("\n");
     fflush(stdout);
     s_old = s;
